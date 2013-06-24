690694f02f2136a9383af61eccd08f01a3f3d8f0
jcran <jcran@pentestify.com>
2013-06-24 18:45:14 -0500

    return OK for every connection

--- server/server.rb
+++ server/server.rb
@@ -3,7 +3,7 @@ require 'socket'
 server = TCPServer.new(2000)
 loop do
   Thread.start(server.accept) do |client|
-    client.puts "Connection made at #{Time.now}"
+    client.puts "OK"
     client.close
   end
 end
