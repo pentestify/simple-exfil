8d7f96047e08b4fd310fb7114427c963c9e63fe5
jcran <jcran@pentestify.com>
2013-06-24 19:09:25 -0500

    naming tweak

--- server/server.rb
+++ server/server.rb
@@ -2,8 +2,8 @@ require 'socket'
 
 server = TCPServer.new(2000)
 loop do
-  Thread.start(server.accept) do |client|
-    client.puts "OK"
-    client.close
+  Thread.start(server.accept) do |socket|
+    socket.puts "OK"
+    socket.close
   end
 end
